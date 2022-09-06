dc462997a52ca13a0a6ff88fcb5ca4517f715026
Thomas Labarussias <issif+github@gadz.org>
2022-09-06 12:13:05 +0200

    refactor
    Signed-off-by: Thomas Labarussias <issif+github@gadz.org>

--- main.go
+++ main.go
@@ -1,83 +1,107 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
+	"encoding/xml"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
+	"path/filepath"
 	"strings"
+
+	"github.com/dustin/go-humanize"
 )
 
-type probe struct {
-	FalcoVersion  string `json:"falco_version"`
-	Architecture  string `json:"architecture"`
-	KervelVersion string `json:"kernel_version"`
-	Hash          string `json:"hash"`
-	CreationDate  string `json:"creation_date"`
-	Size          string `json:"size"`
-	Link          string `json:"link"`
+const source = "https://falco-distribution.s3-eu-west-1.amazonaws.com/?list-type=2&prefix=driver"
+const download = "https://download.falco.org/"
+
+type ListBucketResult struct {
+	XMLName               xml.Name  `xml:"ListBucketResult"`
+	Contents              []Content `xml:"Contents"`
+	NextContinuationToken string    `xml:"NextContinuationToken"`
+	IsTruncated           string    `xml:"IsTruncated"`
 }
 
-type entry struct {
-	FalcoVersion string `json:"falco_version"`
-	Link         string `json:"link"`
+type Content struct {
+	Lib          string `json:"lib"`
+	Arch         string `json:"arch"`
+	Kind         string `json:"kind"`
+	Key          string `xml:"Key" json:"name"`
+	Download     string `json:"download"`
+	Size         int    `xml:"Size"`
+	SizeString   string `json:"size"`
+	LastModified string `xml:"LastModified" json:"lastmodified"`
 }
 
-const baseURL = "https://thomas.labarussias.fr/falco-probes/data/"
-const fileURL = "https://s3.amazonaws.com/download.draios.com/stable/sysdig-probe-binaries/index.html"
-const filePath = "./input/list.html"
+var drivers []Content
 
 func main() {
-	resp, err := http.Get(fileURL)
+	fetchXML("")
+
+	j, err := json.Marshal(drivers)
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = ioutil.WriteFile("./data/list.json", j, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fetchXML(after string) {
+	url := source
+	if after != "" {
+		url += "&start-after=" + after
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	defer resp.Body.Close()
 
-	regx := regexp.MustCompile("\"falco-probe-.*\"")
-	probes := make(map[string][]probe)
-	// scanner := bufio.NewScanner(file)
-	scanner := bufio.NewScanner(resp.Body)
-	for scanner.Scan() {
-		// match := regx.FindAllString(scanner.Text(), -1)
-		match := regx.FindStringSubmatch(scanner.Text())
-		if len(match) != 0 {
-			r := strings.Split(scanner.Text(), " ")
-			// fmt.Printf("%#v\n", r)
-			s := strings.Split(match[0][1:len(match[0])-4], "-")
-			p := probe{
-				FalcoVersion:  s[2],
-				Architecture:  s[3],
-				Hash:          s[len(s)-1],
-				CreationDate:  r[8] + " " + r[9],
-				Size:          r[11],
-				KervelVersion: "",
-				Link:          "https://s3.amazonaws.com/download.draios.com/stable/sysdig-probe-binaries/" + match[0][1:len(match[0])-1],
-			}
-			for i := 4; i < len(s)-1; i++ {
-				p.KervelVersion += s[i] + "-"
-			}
-			p.KervelVersion = p.KervelVersion[:len(p.KervelVersion)-1]
-			probes[p.FalcoVersion] = append(probes[p.FalcoVersion], p)
-		}
+	byteValue, _ := ioutil.ReadAll(resp.Body)
+
+	var list ListBucketResult
+	xml.Unmarshal(byteValue, &list)
+
+	if list.Contents[len(list.Contents)-1].Key == after {
+		return
 	}
 
-	v := make(map[string]string)
-	for i := range probes {
-		v[i] = ""
-		j, _ := json.Marshal(probes[i])
-		err := ioutil.WriteFile("./data/"+i+".json", j, 0755)
-		if err != nil {
-			log.Fatal(err)
+	for i := 0; i < len(list.Contents); i++ {
+		k := list.Contents[i].Key
+		var key, lib, arch, kind string
+		s := strings.Split(k, "/")
+		lib = s[1]
+		switch len(s) {
+		case 3:
+			arch = "x86_64"
+			key = s[2]
+		case 4:
+			arch = s[2]
+			key = s[3]
+		default:
+			continue
 		}
+		switch filepath.Ext(key) {
+		case ".o":
+			kind = "ebpf"
+		case ".ko":
+			kind = "kmod"
+		default:
+			kind = "unknown"
+		}
+		drivers = append(drivers, Content{
+			Lib:          lib,
+			Arch:         arch,
+			Key:          key,
+			SizeString:   humanize.Bytes(uint64(list.Contents[i].Size)),
+			LastModified: list.Contents[i].LastModified,
+			Kind:         kind,
+			Download:     download + k,
+		})
 	}
 
-	entries := map[string]string{}
-	for i := range probes {
-		entries[i] = baseURL + i + ".json"
-	}
-	e, _ := json.Marshal(entries)
-	_ = ioutil.WriteFile("./data/index.json", e, 0644)
+	fetchXML(list.Contents[len(list.Contents)-1].Key)
 }
